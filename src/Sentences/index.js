1f534d081511e61f2f161de748112fd8a395eb06
Michael Daines <mdaines@fastmail.com>
2018-05-04 12:55:26 -0400

    Don't limit on Example Sentences page.

--- src/Sentences/index.js
+++ src/Sentences/index.js
@@ -3,7 +3,7 @@ import { formatSentence } from '../helpers';
 
 class Sentences extends Component {
   render() {
-    const sentences = this.props.grammar.calculate('grammar.sentences').slice(0, 10).map((sentence, index) => {
+    const sentences = this.props.grammar.calculate('grammar.sentences').map((sentence, index) => {
       return (
         <li key={`sentence-${index}`}>{formatSentence(sentence, this.props.grammar)}</li>
       );
